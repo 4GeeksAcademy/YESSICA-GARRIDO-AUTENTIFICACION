8b7d84bd1706068e8ca9558031e58a2a7b2942f9
Yessica Garrido <yessigarrido.work@gmail.com>
2026-02-18 12:26:35 +0000

    cambios

--- src/front/pages/Private.jsx
+++ src/front/pages/Private.jsx
@@ -20,7 +20,7 @@ export const Private = () => {
     return (
         <div className="container-sm w-25">
             <div className="row justify-content-md-center">
-                <h1 className="my-4">Bienvenido Heiber</h1>
+                <h1 className="my-4">Bienvenido</h1>
                 <img src="/mi-imagen-2.jpg" className="w-100 rounded-3  mb-3" alt="Mi imagen 2" />
                 
                 <button onClick={logout} className="btn btn-primary">Cerrar sesión</button>
